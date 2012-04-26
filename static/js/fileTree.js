242c5a737febef27264356ac04e2216684a9a2db
Matt Glazar <matt.glazar@gmail.com>
2012-04-26 09:19:27 -0700

    Show glob-matching files under directories

--- static/js/fileTree.js
+++ static/js/fileTree.js
@@ -1,4 +1,8 @@
 define(['models/jobManager', 'commandPoller', 'annotations'], function(jobManager, commandPoller, annotations) {
+  function hasOwn(obj, prop) {
+    return Object.prototype.hasOwnProperty.call(obj, prop);
+  }
+
   var TYPE_DIRECTORY = 'd';
   var TYPE_FILE = 'f';
   var TYPE_UNKNOWN = '?';
@@ -68,13 +72,43 @@ define(['models/jobManager', 'commandPoller', 'annotations'], function(jobManage
     this.$node = $node;
     this.$list = $('<ul>').appendTo($node.empty());  // HACK
     this.entries = [];
+
+    // Each <li> contains a jQuery data object keyed
+    // 'entry'.
+    this.liByName$ = {};
   }
 
   FileTreeView.prototype.setEntries = function(entries) {
     // TODO Super smart algorithms which don't kill the GC by
     // baleting and creating a zillion DOM nodes each update.
-    this.$list.empty();
-    this.$list.append(entries.map(function(entry) {
+    var lis$ = entries.map(this.makeLi$, this);
+
+    this.$list.empty().append(lis$.map(function($li) {
+      // jQuery([...]) only accepts raw DOM nodes as array
+      // elements.  It does not concatenate jQuery objects.
+      return $li.get(0);
+    }));
+
+    this.entries = entries;
+    this.liByName$ = {};
+    this.entries.forEach(function(entry, i) {
+      this.liByName$[entry.name] = lis$[i];
+    }, this);
+  };
+
+  FileTreeView.prototype.clearAnnotations = function() {
+    fileTreeView.$list.find('.expanded').removeClass('expanded');
+  };
+
+  FileTreeView.prototype.annotateNames = function(names) {
+    names.forEach(function(name) {
+      var parts = name.split(/\//g);
+      var $li = this.makeLiPath$(parts);
+      $li.addClass('expanded');
+    }, this);
+  };
+
+  FileTreeView.prototype.makeLi$ = function(entry) {
       var $link;
       if (entry.type === TYPE_DIRECTORY) {
         $link = $('<a>')
@@ -89,10 +123,80 @@ define(['models/jobManager', 'commandPoller', 'annotations'], function(jobManage
         $link = $('<span>').text(entry.name);
       }
 
-      return $('<li>').append($link).get(0);
-    }));
+      var $li = $('<li>').append($link);
+      $li.data('entry', entry);
+      return $li;
+  };
 
-    this.entries = entries;
+  FileTreeView.prototype.getLiUl$ = function($li) {
+    var $ul = $li.children('ul');
+    if ($ul.length === 0) {
+      $ul = $('<ul>').appendTo($li);
+    }
+    return $ul;
+  };
+
+  // `parts` can be either a list of strings (i.e. a path
+  // split on '/') or a list of entries.
+  FileTreeView.prototype.makeLiPath$ = function(parts) {
+    var names = parts.map(function(part) {
+      if (typeof part === 'string') {
+        return part;
+      } else {
+        return part.name;
+      }
+    });
+
+    // Find an existing <li>.
+    var $existing = null;
+    var i = names.length;
+    while (i --> 0) {
+      var name = names.slice(0, i + 1).join('/');
+      if (hasOwn(this.liByName$, name)) {
+        $existing = this.liByName$[name];
+        break;
+      }
+    }
+
+    var consumed = i + 1;
+
+    if (consumed === names.length) {
+      // An <li> at the full path already exists.  (Woot!)
+      return $existing;
+    }
+
+    var entries = parts.map(function(part, i, array) {
+      if (typeof part === 'string') {
+        var isDir = false;  // TODO figure this out somehow?
+        return {
+          type: isDir ? TYPE_DIRECTORY : TYPE_FILE,
+          perms: { },
+          subEntryCount: NaN,
+          owner: null,
+          group: null,
+          size: NaN,
+          mtime: null,
+          name: part
+        };
+      } else {
+        return part;
+      }
+    });
+
+    var $ul = $existing ? this.getLiUl$($existing) : this.$list;
+    var $li;
+    for (i = consumed; i < entries.length; ++i) {
+      $li = this.makeLi$(entries[i]);
+      var fullName = names.slice(0, i + 1).join('/');
+      this.liByName$[fullName] = $li;
+
+      // TODO Insert in sorted order
+      $ul.append($li);
+
+      $ul = this.getLiUl$($li);
+    }
+
+    return $li;
   };
 
   FileTreeView.prototype.getEntryByName$ = function(name) {
@@ -129,18 +233,25 @@ define(['models/jobManager', 'commandPoller', 'annotations'], function(jobManage
   });
 
   annotations.on('reset', function() {
-    fileTreeView.$list.children().removeClass('expanded');
+    fileTreeView.clearAnnotations();
   });
 
   annotations.on('update', function(annotationData) {
+    // XXX Note that we push onto the accumulator, mutating the
+    // array, for efficiency reasons.
     var spans = annotationData.spans || [];
-    spans.forEach(function(spanData) {
-      spanData.annotations.forEach(function (annoData) {
+    var names = spans.reduce(function(acc, spanData) {
+      spanData.annotations.reduce(function(acc, annoData) {
         if (annoData.expansion) {
-          var $li = fileTreeView.getEntryByName$(annoData.expansion);
-          $li.addClass('expanded');
+          acc.push(annoData.expansion);
         }
-      });
-    });
+
+        return acc;
+      }, acc);
+
+      return acc;
+    }, []);
+
+    fileTreeView.annotateNames(names);
   });
 });
